e19853f45d1d124096aa7474f83b7410293bd041
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2023-01-28 22:49:01 +0100

    Add error messages

--- starterOnly/app.js
+++ starterOnly/app.js
@@ -30,13 +30,21 @@ function menudisAppearance() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+let closeBtn = document.querySelector(".close");
+console.log(closeBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener("click", closeModal);
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
